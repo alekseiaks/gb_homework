f7af1475a58e2fd8cdf043e7826fa714de1c7f5e
alekseiaks <81658015+alekseiaks@users.noreply.github.com>
2021-10-08 23:00:48 +0800

    Delete task_3.py

--- lesson_2/task_3.py
+++
@@ -1,9 +0,0 @@
-number_of_month = int(input('Enter the month number: '))
-if number_of_month in (12, 1, 2):
-    print("Winter")
-elif number_of_month in (3, 4, 5):
-    print("Spring")
-elif number_of_month in (6, 7, 8):
-    print("Summer")
-elif number_of_month in (9, 10, 12):
-    print("Fall")
