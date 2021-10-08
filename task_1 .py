804c07df245a890bd2e4c882e38c9455b535e793
alekseiaks <81658015+alekseiaks@users.noreply.github.com>
2021-10-08 22:59:59 +0800

    Delete task_1 .py

--- task_1 .py
+++
@@ -1,4 +0,0 @@
-name = input("Введите Ваше имя: ")
-born_year = int(input("Ваш год рождения?  : "))
-age = 2021 - born_year
-print(f"Здравствуйте {name}! Ваш год рождения : {born_year} лет, Вам : {age} лет")
