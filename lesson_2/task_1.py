8942a3f7c5688e9524543f16352c9132819412a9
alekseiaks <81658015+alekseiaks@users.noreply.github.com>
2021-10-08 22:38:14 +0800

    Delete task_1.py

--- lesson_2/task_1.py
+++
@@ -1,5 +0,0 @@
-data = [1994, True, 'text', ["hello", "world", 1, True], ("hello", "world", 1, True), {"hello", "world", 1, True},
-        {"key_1": 1024, 10: 986, "key_3": False, 4: None}, None, b'text']
-
-for i in (data):
-    print(f'Элемент списка "{i}" соответствует типу данных: {type(i)}')
